16606b048f10a27f6e35d20881f44a4f29b42a9c
Eshrak <uemran2018@gmail.com>
2026-04-01 07:23:23 +0600

    wroking with shop and flight section and make app comoponent

--- src/pages/main/Flight/FlightHome/FlightHome.tsx
+++ src/pages/main/Flight/FlightHome/FlightHome.tsx
@@ -1,16 +1,34 @@
+import AppSection from "@/components/AppSection";
 import FlightDestination from "./FlightDestination";
 import FlightPromotions from "./FlightPromotions";
 import FlightTourPackages from "./FlightTourPackages";
 
 const FlightHome = () => {
-    return (
-        <div>
-            <FlightDestination/>
-            <FlightPromotions/>
-            <FlightTourPackages/>
-            
+  return (
+    <div className="bg-white dark:bg-gray-950">
+      {/* Hero / Destination */}
+      <section className="max-w-7xl mx-auto px-4 pt-12 md:pt-16 lg:pt-20">
+        <FlightDestination />
+      </section>
+
+      {/* Promotions */}
+      <section className="max-w-7xl mx-auto px-4 py-12 md:py-16">
+        <FlightPromotions />
+      </section>
+
+      {/* Tour Packages with soft background */}
+      <section className="bg-gray-50 dark:bg-gray-900 py-12 md:py-16">
+        <div className="max-w-7xl mx-auto px-4">
+          <FlightTourPackages />
         </div>
-    );
+      </section>
+
+      {/* App CTA (final section, more breathing space) */}
+      <section className="pt-16 md:pt-20 lg:pt-24">
+        <AppSection />
+      </section>
+    </div>
+  );
 };
 
 export default FlightHome;
