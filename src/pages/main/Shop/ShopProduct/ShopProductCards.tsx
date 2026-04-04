84f4a2a749dbb376f0e7404ddac603eb32bd9ee5
FardinCSE25 <fardin.cse25@gmail.com>
2026-04-04 22:08:03 +0600

    show more button applied in products of shop page

--- src/pages/main/Shop/ShopProduct/ShopProductCards.tsx
+++ src/pages/main/Shop/ShopProduct/ShopProductCards.tsx
@@ -2,8 +2,15 @@ import { motion } from "framer-motion";
 import type { Product } from "@/types/shop/types.shop";
 import { Link } from "react-router-dom";
 import SectionHeader from "@/components/shop/SectionHeader";
+import type { Dispatch, SetStateAction } from "react";
 
-const ShopProductCards = ({ products }: { products: Product[] }) => {
+interface ShopProductCardsProps {
+  products: Product[];
+  offset: Dispatch<SetStateAction<number>>;
+}
+
+const ShopProductCards = ({ products, offset }: ShopProductCardsProps) => {
+  
   return (
     <section className="">
       <SectionHeader
@@ -98,6 +105,33 @@ const ShopProductCards = ({ products }: { products: Product[] }) => {
             </Link>
           </motion.div>
         ))}
+      </div>
+
+      {/* Static Show More Button */}
+      <div className="mt-16 flex justify-center pb-8">
+        <button onClick={() => offset((prev) => prev + 8)} className="flex items-center gap-3 px-8 py-4 bg-primary dark:bg-gray-900 border-2 border-gray-100 dark:border-gray-800 text-white hover:opacity-90 rounded-full font-bold dark:hover:bg-gray-800 transition-colors shadow-sm">
+          <svg
+            className="animate-spin h-5 w-5 text-primary hidden" 
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+          Show More Products
+        </button>
       </div>
     </section>
   );
