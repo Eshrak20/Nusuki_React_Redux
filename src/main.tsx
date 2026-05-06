4878e81d1ef573135c537fbbf060caf1416a9a09
Eshrak20 <uemran2018@gmail.com>
2026-05-07 05:47:17 +0600

    working on login and signup

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./routes/Routes";
+import { router } from "./routes/PublicOnlyRoute";
 import { store, persistor } from "./redux/store";
 import { Provider as ReduxProvider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
